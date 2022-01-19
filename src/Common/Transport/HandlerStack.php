d76865b11411e38795cd226145efcf5666af870f
promsters <tomasz.ptakk@gmail.com>
2022-01-19 16:59:12 +0100

    Update HandlerStack.php

--- src/Common/Transport/HandlerStack.php
+++ src/Common/Transport/HandlerStack.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace OpenStack\Common\Transport;
 
 use GuzzleHttp\HandlerStack as GuzzleStack;
-use GuzzleHttp\Utils;
+use function GuzzleHttp\choose_handler;
 
 class HandlerStack extends GuzzleStack
 {
     public static function create(?callable $handler = null): GuzzleStack
     {
-        $stack = new self($handler ?: Utils::chooseHandler());
+        $stack = new self($handler ?: choose_handler());
         $stack->push(Middleware::httpErrors(), 'http_errors');
         $stack->push(Middleware::prepareBody(), 'prepare_body');
 
